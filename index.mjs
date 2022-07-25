84b384b2c0bddf12f409603996f7cc271619e52d
Francis Timilehin Jeremiah <francis.jeremiah@wazuh.com>
2022-07-25 21:07:17 +0300

    JavaScript added
    JavaScript was added with some modifications to id names. This was made to give the Redactr app some functionality.
    
    Co-Authored-By: Haleemat Adetoro <108614534+HaleematAdetoro@users.noreply.github.com>

--- index.mjs
+++ index.mjs
@@ -1,3 +1,46 @@
-import startApp from './app.mjs';
+// import startApp from './app.mjs';
 
-document.addEventListener('DOMContentLoaded', startApp); 
+// document.addEventListener('DOMContentLoaded', startApp); 
+
+let initialMessage = document.getElementById('initial_message')
+let toRedact = document.getElementById('toRedact-id')
+let defaultRedacter = "***"
+const button = document.getElementById('redact-btn')
+button.addEventListener("click", isClicked)
+let counter = 0
+
+function isClicked(){
+  const startTime = performance.now()
+  let messageStrings = initialMessage.value.split(' ')
+  console.log(messageStrings)
+    let redactList = toRedact.value.toLowerCase().split(' ')
+  console.log(redactList)
+  isDefaultRedacter()
+  let charCount = 0
+
+  for (let i in messageStrings){
+    if (redactList.includes(messageStrings[i].toLowerCase()) == true){
+      charCount += messageStrings[i].length
+      messageStrings[i] = defaultRedacter
+      counter++
+      
+    }
+  } 
+  document.getElementById('redacted_message').innerHTML = messageStrings.join(" ")
+  document.getElementById('a').innerHTML = messageStrings.length
+  document.getElementById('b').innerHTML = counter
+  document.getElementById('c').innerHTML = charCount
+  const endTime = performance.now()
+  document.getElementById('d').innerHTML = endTime - startTime
+}
+
+function isDefaultRedacter(){
+  let redacter = document.getElementById('redacter-id')
+  if(redacter.value == ""){
+    console.log(defaultRedacter)
+    return  
+  }else{
+    defaultRedacter = redacter.value
+    console.log(defaultRedacter)
+  }
+}
